9e813fb94b78ef12aa2ed29049e26025ff5a377d
rfloarea <account@rfloarea.net>
2024-09-07 19:49:00 -0400

    event listener for add note

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // select static elements
 const btnAddNote = document.querySelector('#btnAddNote');
 const main = document.querySelector('#main');
+
+// create event listener
+btnAddNote.addEventListener('click', handleAddNote);
+
+function handleAddNote() {
+  console.log('add note');
+}
