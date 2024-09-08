402b120914a053beff10dcba3457ccd644a045cf
rfloarea <account@rfloarea.net>
2024-09-07 20:00:08 -0400

    create and append new notes

--- script.js
+++ script.js
@@ -7,4 +7,31 @@ btnAddNote.addEventListener('click', handleAddNote);
 
 function handleAddNote() {
   console.log('add note');
+  // create a note element
+  const note = document.createElement('div');
+  // give it substance
+  note.classList.add('note');
+  note.innerHTML = 
+  `
+    <div class="actions">
+      <button>SAVE</button>
+      <button>TRASH</button>
+    </div>
+    <div class="title-container">
+      <textarea
+        class="title"
+        placeholder="What is this called?"
+      >
+      </textarea>
+    </div>
+    <div class="content-container">
+      <textarea
+        class="content"
+        placeholder="What do you ache to say?'
+      >
+      </textarea>
+    </div>
+  `;
+  // render it
+  main.appendChild(note);
 }
