9ab8cf83f1c1eaa84938af6080ad85b9a0a2e6d8
rfloarea <account@rfloarea.net>
2024-09-19 15:33:13 -0400

    render function

--- script.js
+++ script.js
@@ -1,77 +1,92 @@
+/**
+ * What's the story here?
+ * 
+ * This is the story of a simple note taking system.
+ * It uses boring technology that is provided by the web platform.
+ * 
+ * Users can create notes, edit notes, and delete notes.
+ * Creating a note also saves the note.
+ */
+
+// Let's give our sad users some things to play with at the start
 const btnAddNote = document.querySelector('#btnAddNote');
 const main = document.querySelector('#main');
-btnAddNote.addEventListener('click', handleAddNote);
+btnAddNote.addEventListener('click', createNote);
 
-function handleAddNote(title='', content='') {
-  const note = document.createElement('div');
-  note.classList.add('note');
-  note.innerHTML = 
-  `
-    <div class="buttons">
-      <button class="btn-save">SAVE</button>
-      <button class="btn-trash">TRASH</button>
-    </div>
-    <div class="container-title">
-      <label>
-        Title
-      </label>
-      <textarea
-        class="title"
-        rows="3"
-      >${title}</textarea>
-    </div>
-    <div class="container-content">
-      <label>
-        Content
-      </label>
-      <textarea
-        class="content"
-        rows="10"
-      >${content}</textarea>
-    </div>
-  `;
-  
-  const btnSave = note.querySelector('.btn-save');
-  const btnTrash = note.querySelector('.btn-trash');
-  const textareaTitle = note.querySelector('.title');
-  btnSave.addEventListener('click', handleSaveNote);
-  btnTrash.addEventListener('click', handleTrashNote);
-  
-  function handleSaveNote() {
-    save();
-  };
+// Let's shape our data and hold onto it
+const notes = [
+  {
+    title: "Note title one",
+    content: "Content of note one",
+  },
+  {
+    title: "Note title two",
+    content: "Content of note two",
+  },
+];
 
-  function handleTrashNote() {
-    note.remove();
-    save();
-  };
-  main.appendChild(note);
-  save();
-  textareaTitle.focus();
+// Render stored data in UI
+if (localStorage.hasOwnProperty('notes')) {
+  // get data
+  const data = JSON.parse(localStorage.getItem('notes'));
+  // push data into array
+  notes.push(data);
+  // render data to UI
+  renderData(notes);
 };
 
-function save() {
+// render data
+function renderData(notes) {
+  for (const note of notes) {
+    console.log(note);
+    const noteCard = document.createElement('div');
+    noteCard.classList.add('note');
+    noteCard.innerHTML = 
+    `
+      <div class="buttons">
+        <button class="btn-save">SAVE</button>
+        <button class="btn-trash">TRASH</button>
+      </div>
+      <div class="container-title">
+        <label>
+          Title
+        </label>
+        <textarea
+          class="title"
+          rows="3"
+        >${note.title}</textarea>
+      </div>
+      <div class="container-content">
+        <label>
+          Content
+        </label>
+        <textarea
+          class="content"
+          rows="10"
+        >${note.content}</textarea>
+      </div>
+    `
+    const btnTrash = noteCard.querySelector('.btn-trash');
+    btnTrash.addEventListener('click', trashNote);
+    main.appendChild(noteCard);
+  };
+}
+
+// refresh notes array and push into local storage
+function saveNote() {
   const notes = document.querySelectorAll('.note .content');
   const titles = document.querySelectorAll('.note .title');
-  const data = [];
+  
   notes.forEach((note, index) => {
-      const content = note.value;
-      const title = titles[index].value;
-      if (content.trim() && title.trim() !== ''){
-          data.push({title, content});
-      };
+    const content = note.value;
+    const title = titles[index].value;
+    if (content.trim() && title.trim() !== ''){
+      notesArray.push({title, content});
+    };
   });
+
   const dataTitles = data.map(item => item.title);
-  localStorage.setItem('dataTitles', JSON.stringify(dataTitles));
   const dataContent = data.map(item => item.content);
+  localStorage.setItem('dataTitles', JSON.stringify(dataTitles));
   localStorage.setItem('dataContent', JSON.stringify(dataContent));
 };
-
-function load() {
-  const dataTitles = JSON.parse(localStorage.getItem('dataTitles'));
-  const dataContent = JSON.parse(localStorage.getItem('dataContent'));
-  for (let i = 0; i < dataTitles.length; i++) {
-    handleAddNote(dataTitles[i], dataContent[i]);
-  };
-};
-load();
