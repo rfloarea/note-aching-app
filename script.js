19914bc903d623297f928a55b8df1f820cd05bdc
rfloarea <account@rfloarea.net>
2024-09-20 13:33:00 -0400

    everything but trash function

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-/**
- * What's the story here?
- * 
- * This is the story of a simple note taking system.
- * It uses boring technology that is provided by the web platform.
- * 
- * Users can create notes, save notes, edit notes, and delete notes.
- */
-
-// Let's give our sad users some things to play with at the start
+// DONE
 const main = document.querySelector('#main');
-
 const btnAddNote = document.querySelector('#btn-add-note');
 const btnSave = document.querySelector('#btn-save');
-
-btnAddNote.addEventListener('click', createNoteCard);
+btnAddNote.addEventListener('click', handleAddNewNote);
 btnSave.addEventListener('click', saveNotes);
 
-// Let's shape our data and hold onto it
-const notes = [
+// DONE
+let data = [
   {
     title: "Note title one",
     content: "Content of note one",
@@ -30,37 +19,9 @@ const notes = [
   },
 ];
 
-// Get data from storage, render it in UI
-if (localStorage.hasOwnProperty('notes')) {
-  // get data
-  const data = JSON.parse(localStorage.getItem('notes'));
-  // push data into array
-  notes.push(data);
-  // render data to UI
-  buildNodeList(notes);
-};
-
-// FOR TESTING
-buildNodeList(notes);
-
-// build our list of elements
-function buildNodeList(notes) {
-  // clear nodelist
-  while (main.firstChild) {
-    main.removeChild(main.firstChild);
-  };
-  // rebuild nodelist
-  for (const note of notes) {
-    // create an element for each note object
-    const noteCard = createNoteCard(note);
-    // append it to main
-    main.appendChild(noteCard);
-  };
-};
-
-function createNoteCard(note) {
+// DONE
+const NoteCard = ({ title, content, id}) => {
   const noteCard = document.createElement('div');
-  const id = Math.random();
   noteCard.setAttribute('id', id);
   noteCard.setAttribute('class', 'note');
   noteCard.innerHTML = 
@@ -73,7 +34,7 @@ function createNoteCard(note) {
       <textarea
         class="title"
         rows="3"
-      >${note.title}</textarea>
+      >${title}</textarea>
     </div>
     <div class="container-content">
       <label>
@@ -82,41 +43,70 @@ function createNoteCard(note) {
       <textarea
         class="content"
         rows="10"
-      >${note.content}</textarea>
+      >${content}</textarea>
     </div>
   `;
   const btnTrash = noteCard.querySelector('.btn-trash');
   btnTrash.addEventListener('click', () => trashNote(id));
-  notes.push(`${note.title}`, `${note.content}`, id);
-
-  console.log(noteCard);
-
-  return noteCard;
+  main.appendChild(noteCard);
 };
 
-function trashNote(id) {
-  console.log(id)
-  const noteCards = document.querySelectorAll('.note');
-  console.log(noteCards)
-  const newNoteCards = Array.from(noteCards).filter(noteCard => noteCard.id != id);
-  buildNodeList(newNoteCards);
+// DONE
+function buildNodeList(data) {
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  };
+  for (const obj of data) {
+    NoteCard(obj)
+  };
 };
 
-// The purpose of this function is to save all notes with one action
+// DONE
+function handleAddNewNote() {
+  const newNote = {
+    title: '',
+    content: '',
+    id: Math.random(),
+  }
+  if (localStorage) {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    const newData = [...notes, newNote];
+    NoteCard(newNote);
+    buildNodeList(newData)
+  } else {
+    NoteCard(newNote);
+  }
+};
+
+// DONE
 function saveNotes() {
-  // first we collect our note elements into an array
   const noteCards = document.querySelectorAll('.note');
-  // now we iterate through that array using a for...of loop (I like the syntax)
+  data = [];
   for (const noteCard of noteCards) {
-    // extract the title, content, and id values
     const title = noteCard.querySelector('.title').value;
     const content = noteCard.querySelector('.content').value;
     const id = noteCard.id;
-    // create an object with those values
-    const note = { title, content, id };
-    // push that object into our notes array (initialized at the top of our program)
-    notes.push(note);
+    const noteData = { title, content, id };
+    data.push(noteData);
   };
-  // now we store our updated notes array
-  localStorage.setItem('notes', JSON.stringify(notes));
+  console.log(data);
+  localStorage.setItem('notes', JSON.stringify(data));
+};
+
+// DONE
+if (localStorage.hasOwnProperty('notes')) {
+  const notes = JSON.parse(localStorage.getItem('notes'));
+  console.log(notes);
+  buildNodeList(notes);
+} else {
+  buildNodeList(data);
+}
+
+// TODO
+function trashNote(id) {
+  console.log(id)
+  // const noteCards = document.querySelectorAll('.note');
+  // console.log(noteCards)
+  // const newNoteCards = Array.from(noteCards).filter(noteCard => noteCard.id != id);
+  // buildNodeList(newNoteCards);
 };
